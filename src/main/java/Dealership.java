8eb7c6c70ae067d7dbfffcbc7724c850a02a1f32
garyb6 <gary.bennett6@outlook.com>
2022-02-15 15:00:53 +0000

    set up dealership

--- src/main/java/Dealership.java
+++ src/main/java/Dealership.java
@@ -1,2 +1,22 @@
-public class Dealership {
+import behaviours.ICollection;
+
+import java.util.ArrayList;
+
+public class Dealership implements ICollection {
+
+    private double till;
+    private ArrayList<ICollection> vehicles;
+
+    public Dealership(double till, ArrayList<ICollection> vehicles) {
+        this.till = till;
+        this.vehicles = vehicles;
+    }
+
+    public double getTill() {
+        return till;
+    }
+
+    public int getVehiclesNumber() {
+        return vehicles.size();
+    }
 }
