81fc6b12df586e15610672b8ff1085ce052fd59f
garyb6 <gary.bennett6@outlook.com>
2022-02-15 14:48:17 +0000

    engine component set up

--- src/main/java/components/Engine.java
+++ src/main/java/components/Engine.java
@@ -3,4 +3,20 @@ package components;
 import behaviours.IComponent;
 
 public class Engine implements IComponent {
+
+    private int cylinders;
+    private int brakeHorsePower;
+
+    public Engine(int cylinders, int brakeHorsePower) {
+        this.cylinders = cylinders;
+        this.brakeHorsePower = brakeHorsePower;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public int getBrakeHorsePower() {
+        return brakeHorsePower;
+    }
 }
