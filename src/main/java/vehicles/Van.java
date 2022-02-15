fd746f321c6aaae8b26788440fcad5efdf270baa
garyb6 <gary.bennett6@outlook.com>
2022-02-15 15:39:06 +0000

    added components to van

--- src/main/java/vehicles/Van.java
+++ src/main/java/vehicles/Van.java
@@ -4,7 +4,13 @@ import behaviours.ICollection;
 import behaviours.IComponent;
 
 public class Van extends Vehicle implements IComponent, ICollection {
-    public Van(String color, double price, String make, String model) {
+
+    private IComponent component;
+    private IComponent component2;
+    
+    public Van(String color, double price, String make, String model, IComponent component, IComponent component2) {
         super(color, price, make, model);
+        this.component = component;
+        this.component2 = component2;
     }
 }
--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -1,18 +1,29 @@
 import behaviours.ICollection;
+import behaviours.IComponent;
+import components.Engine;
+import components.Tyres;
 import org.junit.Before;
 import org.junit.Test;
 import vehicles.HybridCar;
+import vehicles.Van;
 
 import static org.junit.Assert.*;
 
 public class CustomerTest {
 
     Customer customer;
+    Engine engine;
+    Tyres tyres;
     HybridCar hybridCar;
+    Van van;
+
 
     @Before
     public void setUp(){
+        engine = new Engine(6, 150);
+        tyres = new Tyres(4, "hard");
         hybridCar = new HybridCar("red", 2000.00, "Renault", "Leaf");
+        van = new Van("white", 2500.00, "Toyota", "C-class", engine, tyres);
         customer = new Customer (25000.00);
         customer.addVehicle(hybridCar);
 
@@ -24,7 +35,14 @@ public class CustomerTest {
     }
 
     @Test
+    public void canAddVehicle(){
+        customer.addVehicle(van);
+        assertEquals(2, customer.getVehiclesNumber());
+    }
+
+    @Test
     public void canGetVehicleNumber(){
+
         assertEquals(1, customer.getVehiclesNumber());
     }
 }
