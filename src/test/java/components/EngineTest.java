93634b00547cfd1fd91093f91a05bbcb83208148
garyb6 <gary.bennett6@outlook.com>
2022-02-15 14:52:18 +0000

    engine getters tested and passed

--- src/test/java/components/EngineTest.java
+++ src/test/java/components/EngineTest.java
@@ -1,7 +1,26 @@
 package components;
 
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class EngineTest {
 
+    private Engine engine;
+
+    @Before
+    public void setUp(){
+        engine = new Engine(6,75);
+    }
+
+    @Test
+    public void canGetCylinders(){
+        assertEquals(6, engine.getCylinders());
+    }
+
+    @Test
+    public void canGetBrakeHorsePower(){
+        assertEquals(75, engine.getBrakeHorsePower());
+    }
 }
