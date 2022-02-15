026fa33f0fe7198414468a66b4ec850a21c2d062
garyb6 <gary.bennett6@outlook.com>
2022-02-15 16:18:53 +0000

    set up electricCar test

--- src/test/java/vehicles/ElectricCarTest.java
+++ src/test/java/vehicles/ElectricCarTest.java
@@ -1,7 +1,20 @@
 package vehicles;
 
+import components.Engine;
+import components.Tyres;
+import org.junit.Before;
+
 import static org.junit.Assert.*;
 
 public class ElectricCarTest {
+
+    private ElectricCar electricCar;
+    private Engine engine;
+    private Tyres tyres;
+
+    @Before
+    public void setUp(){
+        electricCar = new ElectricCar("red", 2000, "BMW", "4-type", engine, tyres);
+    }
 
 }
